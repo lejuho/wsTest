7e4d9bcf0817267152f5014c9142b8717b69b6c7
lejuho <zedd1324@gmail.com>
2025-03-24 19:47:44 +0900

    cors

--- src/main/java/com/example/wstest/config/SecurityConfig.java
+++ src/main/java/com/example/wstest/config/SecurityConfig.java
@@ -19,7 +19,6 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
-import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -51,7 +50,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Collections.singletonList("*")); // setAllowedOrigins 대신
+        configuration.setAllowedOriginPatterns(Arrays.asList("http://175.118.225.161")); // setAllowedOrigins 대신
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "*"));
         configuration.setExposedHeaders(Arrays.asList("Authorization"));
